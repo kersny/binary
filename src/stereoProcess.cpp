0fdac497a7356f57cb33b936c072e03de8635480
Neal Bhasin <thisisneal@gmail.com>
2013-04-14 20:27:52 -0400

    follows tutorials to get FLANN SIFT matches and show the "good" ones

--- src/stereoProcess.cpp
+++ src/stereoProcess.cpp
@@ -10,6 +10,8 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+
 #include "opencv2/nonfree/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
@@ -29,10 +31,46 @@ void process_im_pair(const cv::Mat& L_mat, const cv::Mat& R_mat, ros::Time t)
 	os << "Processing image pair with timestamp: " << t << std::endl;
 	debug_print(os.str(), 3);
 
+    // Detect SIFT keypoints in both images
+    debug_print("Detecting SIFT keypoints.\n", 3);
 	cv::SiftDescriptorExtractor detector;
     std::vector<cv::KeyPoint> L_kps, R_kps;
     detector.detect(L_mat, L_kps);
     detector.detect(R_mat, R_kps);
+
+
+    // Extract SIFT features
+    debug_print("Extracting SIFT features.\n", 3);
+    cv::SiftDescriptorExtractor extractor;
+    cv::Mat L_features, R_features;
+    extractor.compute( L_mat, L_kps, L_features );
+    extractor.compute( R_mat, R_kps, R_features );
+
+    // Find feature matches between two images
+    debug_print("Matching SIFT features.\n", 3);
+    cv::FlannBasedMatcher matcher;
+    std::vector< cv::DMatch > matches;
+    matcher.match(L_features, R_features, matches );
+
+    // Refine matches (based on min distance, not RANSAC)
+    cv::vector<cv::DMatch> good_matches;
+    double min = 100.0;
+    for(uint i=0; i < L_kps.size(); i++) {
+        if(matches[i].distance < min) {
+            good_matches.push_back(matches[i]);
+        }
+    }
+
+    // Display matches
+    cv::Mat img_matches;
+    cv::drawMatches(L_mat, L_kps, R_mat, R_kps,
+                    good_matches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1),
+                    std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+    cv::Mat matches_small;
+    matches_small = cv::Mat::zeros(img_matches.rows / 3, img_matches.cols / 3, 16);
+    cv::resize(img_matches, matches_small, matches_small.size());
+    cv::namedWindow("Matches", CV_WINDOW_AUTOSIZE); 
+    cv::imshow("Matches" , matches_small);
 
     cv::Mat L_out, R_out;
     cv::drawKeypoints(L_mat, L_kps, L_out, cv::Scalar(255, 0, 0), DRK);
