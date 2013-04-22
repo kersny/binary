5e0e39fa98cb8e2efc4b216eb151b102279ed62f
Neal Bhasin <thisisneal@gmail.com>
2013-04-22 17:11:24 -0400

    tight output pretty print

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -16,13 +16,28 @@ void debug_print(std::string str, uint level) {
 
 // Pretty print Mat of doubles
 std::string ppmd(cv::Mat m) {
+    double abs_max_val = 0.0;
+    for(int i = 0; i < m.rows; i++) {
+        for(int j = 0; j < m.cols; j++) {
+            double abs_cur = abs(m.at<double>(i,j));
+            if(abs_cur > abs_max_val)
+                abs_max_val = abs_cur;
+        }
+    }
+    int width;
+    int prec = 4;
+    if(abs_max_val < 10) width = prec + 2 + 2;
+    else if(abs_max_val >= 10) width = prec + 2 + 3; // decimals, dot, negative sign, digits
+    else if(abs_max_val >= 100) width = prec + 2 + 4; 
+    else width = prec + 2 + 5;
+
     std::stringstream s;
     s << std::setiosflags(std::ios::fixed);
     s << "[";
     for(int i = 0; i < m.rows; i++) {
         if(i > 0) s << " ";
         for(int j = 0; j < m.cols; j++) {
-            s << std::setw(7) << std::setprecision(4) << m.at<double>(i,j);
+            s << std::setw(width) << std::setprecision(prec) << m.at<double>(i,j);
             if(j < m.cols-1) s << " , ";
             else s << " ; ";
         }
